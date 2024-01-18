56832ec5762cd1e1c81113684b55ee4dd8869848
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-01-18 20:53:58 -0300

    Inclusão de texto

---
+++ src/components/login/styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+height: 100vw;
+flex-direction: column;
+gap: 30px;
+
+svg {
+  width: 100px;
+  height: 100px;
+  color: #046ee5;
+}
+`;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import {createRoot} from 'react-dom/client'
 import App from './App';
+import AuthProvider from './contexts/auth';
 
 const root = createRoot(document.querySelector("#root"))
 
-root.render(<App/>);
+root.render(
+  <AuthProvider>
+    <App/>
+  </AuthProvider>
+);
