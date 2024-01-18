b27c0c04538fd2ea4583daf885cc9fef2f8e4ed7
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-01-18 20:08:51 -0300

    Inclusão de texto

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -5,6 +5,28 @@ export const AuthContext = createContext({});
 const AuthProvider = ({ children }) => {
   const {user, setUser} = useState(null);
 
+  useEffect(() => {
+    function loadStorage() {
+      const storageUser = localStorage.getItem("AuthFaceClone");
+
+      if (storageUser) {
+        setUser(JSON.parse(storageUser));
+      }
+    }
+
+    loadStorage();
+  },[]);
+
+  const signInWithFacebook = (res) => {
+    let data = {
+      name: res.name,
+      email: res.email,
+      avatarUrl: res.picture.data.url,
+    };
+
+    setUser(data);
+    localStorage.setItem("AuthFaceClone", JSON.stringify(data));
+  };
 
   return (
     <AuthContext.Provider
