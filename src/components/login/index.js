ef6c796774861037c9719e6752e042a428cc0313
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-01-18 21:00:23 -0300

    Inclusão de texto

---
+++ src/components/login/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactFacebookLogin from "react-facebook-login";
+import { SiFacebook } from "react-icons/si";
+import { AuthContext } from "../../contexts/auth";
+import GlobalStyles from "../../styles/global";
+import * as C from "./styles";
+
+
+const Login = () => {
+
+  return (
+    <>
+    
+    </>
+  );
+};
+
+export default Login;
