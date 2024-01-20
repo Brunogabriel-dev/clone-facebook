52a0c9443c39aec777ad77989a70a8c4b4921c2c
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-01-20 14:54:55 -0300

    Inclusão de texto

---
+++ src/components/Header/index.js
@@ -0,0 +1,21 @@
+import React, { useContext } from "react";
+import * as C from "./styles";
+import { SiFacebook, SiNintendogamecube, SiMessenger } from "react-icons/si";
+import { FiSearch, FiMenu } from "react-icons/fi";
+import {
+  MdOndemandVideo,
+  MdHome,
+  MdGroups,
+  MdNotifications,
+} from "react-icons/md";
+import { FaStore, FaCarretDown } from "react-icons/fa";
+import { CgMenuGridO } from "react-icons/cg";
+
+const Header = () => {
+
+  return (
+
+  );
+};
+
+export default Header;
