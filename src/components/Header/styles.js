68a05d4ff0d75dd39c339f9123d174efffa39278
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-01-20 14:32:18 -0300

    Inclusão de texto

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,19 +1,25 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
- height: 50px;
- background-color: #fff;
- box-shadow: 0 0 3px 1px #ccc;
- display: flex;
- align-items: center;
- justify-content: center;
+  height: 50px;
+  background-color: #fff;
+  box-shadow: 0 0 3px 1px #ccc;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const Context = styled.div`
- display: flex;
- justify-content: space-between;
- align-items: center;
- width: 100%;
- padding: 0 10px;
- gap: 5px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  padding: 0 10px;
+  gap: 5px;
+`;
+
+export const Section = styled.section`
+  display: flex;
+  gap: 10px;
+  width: 100%;
 `;
