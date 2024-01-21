a01740a4e84d1744fb1051a5cde4091d28b77700
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-01-21 12:22:48 -0300

    Inclusão de texto

--- src/components/HeaderIcon/styles.js
+++ src/components/HeaderIcon/styles.js
@@ -10,4 +10,26 @@ padding: 0 25px;
 cursor: pointer;
 display: ${(props) => (props.display === "mobile" ? "none" : "flex")};
 
+&:hover {
+  background-color: #f0f2f5;
+}
+
+svg {
+  width: 22px;
+  height: 22px;
+  color: #65676b;
+
+  &.avtive {
+    color: #1b74e4
+  }
+
+  @media (max-width: 350px) {
+    width: 16px;
+    height: 16px;
+  }
+}
+
+@media (max-width: 825px) {
+  display: ${(props) => (props.display !== "web" ? "flex" : "none")};
+}
 `;
