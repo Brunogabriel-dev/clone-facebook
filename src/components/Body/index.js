92cb7926a44d0044f4cec6bdc5e37d2bb873fcd6
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-01-25 20:56:41 -0300

    Inclusão de texto

---
+++ src/components/Body/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import * as C from "./styles";
+
+const Body = () => {
+  return (
+    <C.Container>
+      {/*
+      <Shortcuts />
+      <Feed />
+      <Contacts />
+       */}
+    </C.Container>
+  );
+};
+
+ export default Body;
+
