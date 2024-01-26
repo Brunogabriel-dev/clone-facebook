d9d9102c57b7a5b78d2c08bfccc49fac98136856
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-01-25 22:27:29 -0300

    Inclusão de texto

--- src/components/Shortcuts/index.js
+++ src/components/Shortcuts/index.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
+import ShortcutItem from "../ShortcutItem";
+import { FaUserFriends, FaStore } from "react-icons/fa";
+import { MdGroups, MdOndemanVideo } from "react-icons/md";
+import { GiBackwardTime } from "react-icons/gi";
+import { BsChevronDown } from "react-icons/bs";
 
 import * as C from "./styles"
 
 const Shortcuts = () => {
-  return <C.Container></C.Container>
+  return ( 
+    <C.Container>
+
+   </C.Container>
+  );
 };
 
 export default Shortcuts;
