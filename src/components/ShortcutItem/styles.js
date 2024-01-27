6d2c5e2d8958da9d73aef1cdc133fa07292a4283
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-01-26 21:34:46 -0300

    Inclusão de texto

---
+++ src/components/ShortcutItem/styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  padding: 10px;
+  font-size: 14px;
+  cursor: pointer;
+  font-weight: 500;
+  border-radius: 8px;
+  width: 98%;
+`
--- src/components/Shortcuts/index.js
+++ src/components/Shortcuts/index.js
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import ShortcutItem from "../ShortcutItem";
 import { FaUserFriends, FaStore } from "react-icons/fa";
-import { MdGroups, MdOndemanVideo } from "react-icons/md";
+import { MdGroups, MdOndemandVideo } from "react-icons/md";
 import { GiBackwardTime } from "react-icons/gi";
 import { BsChevronDown } from "react-icons/bs";
 import * as C from "./styles";
@@ -9,16 +9,16 @@ import { AuthContext } from "../../contexts/auth";
 
 const Shortcuts = () => {
   const { user } = useContext(AuthContext);
+
   return ( 
     <C.Container>
       <ShortcutItem Profile={user.avatarUrl} Title={user.name} />
       <ShortcutItem Icon={FaUserFriends} Title="Amigos" />
-      <ShortcutItem Icon={FaUserFriends} Title="Grupos" />
-      <ShortcutItem Icon={FaUserFriends} Title="Marketplace" />
-      <ShortcutItem Icon={FaUserFriends} Title="Watch" /> 
-      <ShortcutItem Icon={FaUserFriends} Title="Lembranças" />
-      <ShortcutItem Icon={FaUserFriends} Title="Ver mais" colorIcon="gray" />
-
+      <ShortcutItem Icon={MdGroups} Title="Grupos" />
+      <ShortcutItem Icon={FaStore} Title="Marketplace" />
+      <ShortcutItem Icon={MdOndemandVideo} Title="Watch" /> 
+      <ShortcutItem Icon={GiBackwardTime} Title="Lembranças" />
+      <ShortcutItem Icon={BsChevronDown} Title="Ver mais" colorIcon="gray" />
    </C.Container>
   );
 };
