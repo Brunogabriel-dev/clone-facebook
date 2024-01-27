df25d3a57906b395eb28a8925ece6a27473e164a
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-01-27 13:37:57 -0300

    Inclusão de texto

---
+++ src/components/Contacts/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ContactItem from "../ContactItem";
+import * as C from "./styles";
+import { FiSearch } from "react-icons/fi";
+import { RivideoAddFill } from "react-icons/ri";
+import { GokebabHorizontal } from "react-icons/go";
+
+import bezos from "../../images/bezos.jpg";
+import bezos from "../../images/bill.jpg";
+import bezos from "../../images/mark.jpg";
+import bezos from "../../images/elon.jpg";
+
+
+const Contacts = () => {
+  return (
+
+  );
+};
+
+export default Contacts;
+ 
