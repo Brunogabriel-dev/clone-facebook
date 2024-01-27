cb151933b20e761fcb7a23f93fe88f8a19d9e781
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-01-27 14:38:15 -0300

    Inclusão de texto

---
+++ src/components/ContactItem/styles.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+ export const Container = styled.div`
+   display: flex;
+   align-items: center;
+   gap: 10px;
+   padding: 12px;
+   font: 12px;
+   cursor: pointer;
+   font-weight: 500;
+   border-radius: 8px;
+   width: 98%;
+
+   &:hover {
+    background-color: #0000000d;
+   }
+
+   svg {
+    width: 20px;
+    height: 20px;
+   }
+ `;
+
+ export const Image = styled.img`
+   width: 30px;
+   height: 30px;
+   border-radius: 50%;
+ 
+ `
