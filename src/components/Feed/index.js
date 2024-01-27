f2800beaffa9c054f740e0d9e65be8808eb48f0e
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-01-27 15:19:22 -0300

    Inclusão de texto

---
+++ src/components/Feed/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import * as C from "./styles";
+
+const Feed = () => {
+  <C.Container>
+    {/* <Stories />
+    <NewPost />
+    <Post />
+     */}
+  </C.Container>
+}
