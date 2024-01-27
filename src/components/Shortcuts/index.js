1e78cd842650f5eeda93975bf3dfb1a7c3faedf6
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-01-26 21:09:48 -0300

    Inclusão de texto

--- src/components/Shortcuts/index.js
+++ src/components/Shortcuts/index.js
@@ -4,12 +4,20 @@ import { FaUserFriends, FaStore } from "react-icons/fa";
 import { MdGroups, MdOndemanVideo } from "react-icons/md";
 import { GiBackwardTime } from "react-icons/gi";
 import { BsChevronDown } from "react-icons/bs";
-
-import * as C from "./styles"
+import * as C from "./styles";
+import { AuthContext } from "../../contexts/auth";
 
 const Shortcuts = () => {
+  const { user } = useContext(AuthContext);
   return ( 
     <C.Container>
+      <ShortcutItem Profile={user.avatarUrl} Title={user.name} />
+      <ShortcutItem Icon={FaUserFriends} Title="Amigos" />
+      <ShortcutItem Icon={FaUserFriends} Title="Grupos" />
+      <ShortcutItem Icon={FaUserFriends} Title="Marketplace" />
+      <ShortcutItem Icon={FaUserFriends} Title="Watch" /> 
+      <ShortcutItem Icon={FaUserFriends} Title="Lembranças" />
+      <ShortcutItem Icon={FaUserFriends} Title="Ver mais" colorIcon="gray" />
 
    </C.Container>
   );
