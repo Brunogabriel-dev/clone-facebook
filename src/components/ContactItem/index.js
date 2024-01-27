1c84fc0a5843e98d643b0fa99a7f96ac74bd4d0e
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-01-27 14:41:50 -0300

    Inclusão de texto

---
+++ src/components/ContactItem/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import * as C from "./styles";
+
+const ContactItem = ({ src, name }) => {
+  return (
+    <C.Container>
+    </C.Container>
+  );
+};
+
+export default ContactItem;
+
