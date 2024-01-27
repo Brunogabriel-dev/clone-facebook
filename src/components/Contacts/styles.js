6354f80d5f86b38808cf130dc9a5c2f483420f59
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-01-27 13:13:36 -0300

    Inclusão de texto

---
+++ src/components/Contacts/styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  max-width: 300px;
+  width: 15%;
+  margin-right: 20px;
+
+  @media (max-width: 875px) {
+    display: none;
+  }
+
+`
