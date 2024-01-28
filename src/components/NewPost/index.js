006ed3d5ff154f3e9b35de4a13588606c329b326
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-01-28 12:17:47 -0300

    Inclusão de inclusão

--- src/components/NewPost/index.js
+++ src/components/NewPost/index.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useContext, useRef,useState } from "react";
+import * as C from "./styles";
+import { MdVideoCall, MdCameraAlt} from "react-icons/md";
+import { BiHappyAlt } from "react-icons/bi";
+import { FaTrash } from "react-icons/fa";
+import { db, storage } from "../../firebase";
+import { AuthContext } from "../../contexts/auth";
+import firebase from "firebase/compat/app";
+
 
 const NewPost = () => {
-  return <C.Contaner></C.Contaner>
+  const { user } = useContext(AuthContext);
+  const [desc, setDesc] = useState("");
+  const fileRef = useRef(null);
+  const [filePost, setFilePost] = useState("");
+
+
+
+
+  return ( 
+  <C.Container>
+  </C.Container>
+  );
 };
 
 export default NewPost;
