b5119a1f528a13a86bcb6755bbe3e524a1ebedf9
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-01-27 21:36:58 -0300

    Inclusão de texto

--- src/components/Stories/index.js
+++ src/components/Stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import StoryCard from "../StoryCard";
 import * as C from "./styles";
 
@@ -10,11 +10,26 @@ import elon from "../../images/elon.jpg";
 import { AuthContext } from "../../contexts/auth";
 
 const Stories = () => {
+  const { user } = useContext(AuthContext);
 
+  const itens = [
+    {src: bruno, profile: bruno, name: user.name },
+    {src: bezos, profile: bezos, name: "Jeff Bezoz"},
+    {src: bill, profile: bill, name: "Bill Gates"},
+    {src: mark, profile: mark, name: "Mark Zuckerberg"},
+    {src: elon, profile: elon, name: "Elon Musk"},
+  ];
 
   return (
     <C.Container>
-
+      {itens.map((item, index) => (
+        <StoryCard
+        key={index}
+        src={item.src}
+        name={item.name}
+        profile={item.profile}
+        />
+      ))}
     </C.Container>
   );
 };
