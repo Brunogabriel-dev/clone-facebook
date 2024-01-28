5f3877df2e726d745aaad4cb2f76d46b9cba934d
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-01-27 21:16:29 -0300

    Inclusão de texto

---
+++ src/components/Stories/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import StoryCard from "../StoryCard";
+import * as C from "./styles";
+
+import bruno from "../../images/bruno.jpg";
+import bezos from "../../images/bezos.jpg";
+import bill from "../../images/bill.jpg";
+import mark from "../../images/mark.jpg";
+import elon from "../../images/elon.jpg";
+import { AuthContext } from "../../contexts/auth";
+
+const Stories = () => {
+
+
+  return (
+    <C.Container>
+
+    </C.Container>
+  );
+};
+
+export default Stories;
+
+
