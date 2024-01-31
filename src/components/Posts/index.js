b7c0bbcf1703fcfcc9fa2905aa5ae1d003589c11
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2024-01-31 20:24:35 -0300

    Inclusão de texto

---
+++ src/components/Posts/index.js
@@ -0,0 +1,13 @@
+import React, { useContext } from "react";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { AuthContext } from "../../contexts/auth";
+import { db } from "../../firebase";
+import Post from "../Post";
+
+const Post = () => {
+  return (
+    <>
+
+    </>
+  );
+};
